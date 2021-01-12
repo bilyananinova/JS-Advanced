b34d7d4317ea248da212f611ed84271d9c787df9
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-01-12 14:09:01 +0200

    add Largest Number

---
+++ Syntax, Functions and Statements/largestNumber.js
@@ -0,0 +1,10 @@
+function largestNumber(a, b, c) {
+
+    let number = Math.max(a, b, c);
+    console.log(`The largest number is ${number}.`);
+
+}
+
+largestNumber(5, -3, 16);
+console.log('-----');
+largestNumber(-3, -5, -22.5);
