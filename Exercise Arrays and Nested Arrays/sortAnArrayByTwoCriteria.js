9d9b65a8a32117705ae5138f3d1523450ecb1b05
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-01-16 18:56:06 +0200

    add Sort an Array by 2 Criteria

---
+++ Exercise Arrays and Nested Arrays/sortAnArrayByTwoCriteria.js
@@ -0,0 +1,9 @@
+function sortAnArrayByTwoCriteria(array) {
+    array = array.sort((a, b) => a.length - b.length || a.localeCompare(b));
+
+    console.log(array.join('\n'));
+}
+
+sortAnArrayByTwoCriteria(['alpha', 'beta', 'gamma']);
+sortAnArrayByTwoCriteria(['Isacc', 'Theodor', 'Jack', 'Harrison', 'George']);
+sortAnArrayByTwoCriteria(['test', 'Deny', 'omen', 'Default']);
