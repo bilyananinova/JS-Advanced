46a669c0af9eab121a66f716f1fe0f88682aeb1b
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-01-19 10:12:24 +0200

    add City Record

---
+++ Objects and Composition/cityRecord.js
@@ -0,0 +1,17 @@
+function cityRecord(name, population, treasury) {
+    let city = {};
+
+    city = {
+        name,
+        population,
+        treasury,
+    }
+
+    return city;
+}
+
+console.log(cityRecord('Tortuga', 7000, 15000));
+
+console.log('-------------');
+
+console.log(cityRecord('Santo Domingo', 12000, 23500));
