8061d9a63ab997c2e66cb906d5dd1a571b954acf
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-01-19 10:13:19 +0200

    add Town Population

---
+++ Objects and Composition/townPopulation.js
@@ -0,0 +1,43 @@
+function townPopulation(input) {
+    let towns = {};
+
+    for (const city of input) {
+        let [name, population] = city.split(' <-> ');
+        population = Number(population);
+
+        if (!towns[name]) {
+            towns[name] = 0;
+        }
+
+        towns[name] += population;
+
+    }
+
+    for (const town of Object.keys(towns)) {
+        console.log(`${town} : ${towns[town]}`);
+    }
+    
+}
+
+townPopulation(
+    [
+        'Sofia <-> 1200000',
+        'Montana <-> 20000',
+        'New York <-> 10000000',
+        'Washington <-> 2345000',
+        'Las Vegas <-> 1000000'
+    ]
+);
+
+console.log();
+
+townPopulation(
+    [
+        'Istanbul <-> 100000',
+        'Honk Kong <-> 2100004',
+        'Jerusalem <-> 2352344',
+        'Mexico City <-> 23401925',
+        'Istanbul <-> 1000'
+    ]
+
+);
