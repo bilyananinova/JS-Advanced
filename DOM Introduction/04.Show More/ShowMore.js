e36dbca2772b6a3dda27600fd61da3fdb3ce5ef0
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-01-23 14:31:18 +0200

    add Show More

---
+++ DOM Introduction/04.Show More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    let add = document.getElementById('text');
+    add.style.display = 'inline';
+    let more = document.getElementById('more');
+    more.style.display = 'none'
+}
