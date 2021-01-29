15fb4cef4a5ba6e4ad79c2f9b57e93378f879f14
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-01-29 22:52:39 +0200

    update Pascal or Camel Case

--- Exercise DOM Introduction/02. Pascal or Camel Case/solution.js
+++ Exercise DOM Introduction/02. Pascal or Camel Case/solution.js
@@ -2,26 +2,26 @@ function solve() {
 
   let text = document.getElementById('text').value.toLowerCase().split(' ')
   let type = document.getElementById('naming-convention').value;
-  let result = [];
+  let result = '';
   if (type == 'Camel Case') {
 
     text.forEach((element, index) => {
-      let word = element
       if (index != 0) {
-        word = element[0].toUpperCase() + element.slice(1);
+        result += element[0].toUpperCase() + element.slice(1);
+      } else {
+        result += element;
       }
-      result.push(word);
     });
 
   } else if (type == 'Pascal Case') {
     text.forEach(element => {
 
-      let word = element[0].toUpperCase() + element.slice(1);
-      result.push(word);
+      result += element[0].toUpperCase() + element.slice(1);
+
     });
   } else {
-    result.push('Error!')
+    result = 'Error!'
   }
 
-  document.getElementById('result').textContent = result.join('')
+  document.getElementById('result').textContent = result
 }
