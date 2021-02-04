e4faad2c8bcbedd7119cd93933b19ff75492496f
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-02-04 20:56:05 +0200

    add Get Persons

---
+++ Classes/getPersons.js
@@ -0,0 +1,25 @@
+function getPersons() {
+    class Person {
+        constructor(firstName, lastName, age, email) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.email = email;
+        }
+
+        toString() {
+            return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
+        };
+
+
+    }
+
+    return [
+        person = new Person('Anna', 'Simpson', 22, 'anna@yahoo.com'),
+        person = new Person('SoftUni'),
+        person = new Person('Stephan', 'Johnson', 25),
+        person = new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com'),
+    ]
+}
+
+console.log(getPersons())
