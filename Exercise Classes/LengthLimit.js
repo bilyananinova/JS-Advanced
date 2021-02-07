5ef5a563aa15d7ed26513695b4d9b8a89337dbdc
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-02-07 13:52:03 +0200

    update Length Limit

--- Exercise Classes/LengthLimit.js
+++ Exercise Classes/LengthLimit.js
@@ -16,7 +16,13 @@ class Stringer {
     }
 
     toString() {
-        return this.innerLength !== 0 ? this.innerString.slice(0, this.innerLength) + '...' : '...'
+        if (this.innerString.length <= this.innerLength) {
+            return this.innerString
+        } else if (this.innerLength == 0) {
+            return '...'
+        } else {
+            return this.innerString.slice(0, this.innerLength) + '...'
+        }
     }
 }
 
