cbe10f3ae8264ef6e6059ce78f9fd6a1fcbef964
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-02-17 17:13:15 +0200

    add Class Hierarchy

---
+++ Prototypes and Inheritance/classHierarchy.js
@@ -0,0 +1,69 @@
+function Circle() {
+
+    class Figure{
+        constructor(units = 'cm') {
+            this.units = units;
+        }
+
+        get area(){
+            throw new Error('Not Implemented!');
+        }
+
+        changeUnits(newUnits) {
+            this.units = newUnits;
+        }
+
+        transformMetric(valInCM){
+            return this.units ==='m'? valInCM/100 : this.units ==='mm' ? valInCM * 10 : 
+            valInCM
+        }
+        toString(){
+            return `Figures units: ${this.units} Area: ${this.area}`;
+        }
+    }
+
+    class Circle extends Figure{
+        constructor(radius, units){
+            super(units);
+            this.radius = radius;
+        }
+
+        get area(){
+            const radius =this.transformMetric(this.radius);
+            return Math.PI * radius * radius;
+        }
+
+        toString(){
+            const r = this.transformMetric(this.radius);
+            return `${super.toString()} - radius: ${r}`;
+        }
+    }
+
+    class Rectangle extends Figure{
+        constructor (width, height, units) {
+            super(units);
+            this.width = width;
+            this.height = height;
+        }
+
+        get area(){
+            const width = this.transformMetric(this.width);
+            const height = this.transformMetric(this.height);
+            return height * width;
+        }
+
+        toString(){
+            const width = this.transformMetric(this.width);
+            const height = this.transformMetric(this.height);
+            return `${super.toString()} - width: ${width}, height: ${height}`;
+        }
+    }
+    
+    return{
+        Figure,
+        Circle,
+        Rectangle
+    };
+
+
+}
